df5bd3733a3730f84270dce7c6f7daf145caf15f
Nipun Sud <nipunsud123@gmail.com>
2017-04-09 22:42:21 -0400

    Update to QuestionCtrl adding fem workflow for notifications

--- src/main/java/com/zazen/infrastructure/v1/controller/QuestionController.java
+++ src/main/java/com/zazen/infrastructure/v1/controller/QuestionController.java
@@ -1,7 +1,9 @@
 package com.zazen.infrastructure.v1.controller;
 
+import java.io.IOException;
 import java.util.List;
 
+import org.apache.http.client.ClientProtocolException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,14 +11,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.zazen.infrastructure.v1.pojos.Message;
+import com.zazen.infrastructure.v1.pojos.Notification;
 import com.zazen.infrastructure.v1.pojos.Question;
 import com.zazen.infrastructure.v1.pojos.User;
 import com.zazen.infrastructure.v1.repository.QuestionRepository;
 import com.zazen.infrastructure.v1.repository.UserRepository;
+import com.zazen.infrastructure.v1.service.FcmService;
 import com.zazen.infrastructure.v1.service.QuestionService;
 import com.zazen.infrastructure.v1.vo.QuestionRequestVO;
 
@@ -35,6 +39,9 @@ public class QuestionController {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private FcmService fcmService;
 	
 	
 	@RequestMapping(value = "/question", method = RequestMethod.POST, headers = "Accept=application/json")
@@ -55,7 +62,7 @@ public class QuestionController {
 		//#TODO use a cron job to run and get users around the location of the question asked
 		// From those users find the registration Id and using fcm service send notifications to them 
 		// Maybe need a status enum?
-		
+		sendMessage(questionRequest.getQuery());
 		List<User> usersAroundLocation = questionService.getUsersAroundQuestion(question);
 		//Send question to these users using fcm service
 		return questionRespository.save(question);
@@ -95,6 +102,27 @@ public class QuestionController {
 	public List<Question> listQuestionsByLocation(@PathVariable("lat") Long longitude,@PathVariable("long") Long latitude){
 		List<Question> questions = questionRespository.findAllByLocation(longitude, latitude);
 		return questions;		
+	}
+	
+	public void sendMessage(String query){
+		Message message = new Message();
+		//phone id - "fXaM6Dic1nI:APA91bE7-bxvj4JVgj4cBK9GG2QKr8fsYG6tVAWm9jskO7R6tzWSGHODOGLhj2YkMoIa0tHKQ1HqUn10zDhyqCaMckuuQWWO0bAtZDijUE0Q-RsU7GAhayNuAAU54IRmo1Z01Bi8gzwA"
+		message.setTo("e_4bc0aoIdg:APA91bH6aoxf_Rkte6B6Zd0hI9EpIH9_0DTyDrfl36vuLz1odo_-BqKwMfqcIBOemmRBqY6vyDjSWksUrmhY1L6Nmgc7t1cq0Bxa5ARIPWrb-UK0uAVyB0ncvktklzg0tg1Fb6CW9o-e");
+		message.setPriority("high");
+		Notification note = new Notification();
+		note.setBody(query);
+		note.setTitle("Test Me");
+		message.setNotification(note);
+		try {
+			fcmService.sendFcmMessage(message);
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//message/
 	}
 	
 }
--- src/main/java/com/zazen/infrastructure/v1/service/FcmService.java
+++ src/main/java/com/zazen/infrastructure/v1/service/FcmService.java
@@ -1,6 +1,8 @@
 package com.zazen.infrastructure.v1.service;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
@@ -8,14 +10,17 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.json.JSONArray;
 import org.json.JSONObject;
+import org.springframework.stereotype.Component;
 
 import com.zazen.infrastructure.v1.pojos.Message;
 
+@Component
 public class FcmService {
 	
 	private static String FCM_URL = "https://fcm.googleapis.com/fcm/send";
-	private static String SERVER_KEY = "key=AAAAmoa49IE:APA91bFWvyzF0pAaZoTaaE7Srvldui7z4PUoxdMZui_Qe38QY9vAQGdvMPmb43TlHItLZnUTz7FTZSqz9D5Hbm1B7-REufrqNLzwfsSgPGoHeoa3Hz856XB_3LQ9jHezVdWO29FiPPu5";
+	private static String SERVER_KEY = "key=AAAALFsjD1Q:APA91bHONY85PMG3TmP9E754P6n-7jGvVoFQonKpAgrJiS5S_QcNeGBI7-FPRgIVR_uic3R5-XXmKiyuVm2Qd_5uDSQMdWRMFenxESerDHGPH1cWMD174OFrnPAa5qXTMIoSX0OPIABc";
 	
 	
 	public void sendFcmMessage(Message message) throws ClientProtocolException, IOException{
@@ -25,18 +30,30 @@ public class FcmService {
 		post.setHeader("Authorization", SERVER_KEY);
 
 		JSONObject messageToSend = new JSONObject();
-		messageToSend.put("to", message.getTo());
-		messageToSend.put("priority", message.getPriority());
-
+		JSONArray registerationIds = new JSONArray();
+		registerationIds.put(message.getTo());
 		JSONObject notification = new JSONObject();
 		notification.put("title", message.getNotification().getTitle());
 		notification.put("body", message.getNotification().getBody());
 
-		messageToSend.put("notification",  message.getNotification());
+		messageToSend.put("notification",  notification);
+		messageToSend.put("registration_ids", registerationIds);
+		messageToSend.put("priority", message.getPriority());
+		//messageToSend.put("content-available","1");
+
+		
 
 		post.setEntity(new StringEntity(messageToSend.toString(), "UTF-8"));
 		HttpResponse response = client.execute(post);
-		System.out.println(response);
+		BufferedReader rd = new BufferedReader(
+		        new InputStreamReader(response.getEntity().getContent()));
+
+		StringBuffer result = new StringBuffer();
+		String line = "";
+		while ((line = rd.readLine()) != null) {
+			result.append(line);
+		}
+		System.out.println("Here is the response"+ result.toString());
 		System.out.println(messageToSend);
 	}
 }
