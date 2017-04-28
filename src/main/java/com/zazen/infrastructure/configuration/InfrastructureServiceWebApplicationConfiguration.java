c8a07dc7004addc106c423ea3c9ff24319518b0c
Nipun Sud <nipunsud123@gmail.com>
2017-04-28 18:10:02 -0400

    using was server

--- src/main/java/com/zazen/infrastructure/configuration/InfrastructureServiceWebApplicationConfiguration.java
+++ src/main/java/com/zazen/infrastructure/configuration/InfrastructureServiceWebApplicationConfiguration.java
@@ -1,16 +1,10 @@
 package com.zazen.infrastructure.configuration;
 
-import java.net.InetSocketAddress;
+import java.sql.SQLException;
 import java.util.Properties;
 
 import javax.sql.DataSource;
 
-import org.elasticsearch.client.Client;
-import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.settings.Setting;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.transport.InetSocketTransportAddress;
-import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -32,44 +26,47 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @EnableWebMvc
 @EnableTransactionManagement
 @ComponentScan(basePackages="com.zazen.infrastructure")
-@PropertySource( "classpath:application.properties")
+@PropertySource(value={"file://yuvo/conf/application.properties"})
+//"classpath:application.properties"})
 
 public class InfrastructureServiceWebApplicationConfiguration extends WebMvcConfigurerAdapter{
 
 	@Autowired
-    private Environment environment;
- 
-    @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
-        dataSource.setUrl(environment.getRequiredProperty("jdbc.url"));
-        dataSource.setUsername(environment.getRequiredProperty("jdbc.username"));
-        dataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
-        return dataSource;
-    }
-    
- 
-    /*
-     * Here you can specify any provider specific properties.
-     */
-    private Properties hibernateProperties() {
-        Properties properties = new Properties();
-        properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
-        properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
-        properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
-        properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
-        return properties;
-    }
- 
-    @Bean
-    @Autowired
-    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
-        HibernateTransactionManager txManager = new HibernateTransactionManager();
-        txManager.setSessionFactory(sessionFactory);
-        return txManager;
-    }
-	
+	private Environment environment;
+
+	@Bean
+	public DataSource dataSource() throws ClassNotFoundException, SQLException {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		Class.forName("com.mysql.jdbc.Driver");
+		//        dataSource.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
+		dataSource.setUrl(environment.getRequiredProperty("RDS_HOSTNAME"));
+		dataSource.setUsername(environment.getRequiredProperty("RDS_USERNAME"));
+		dataSource.setPassword(environment.getRequiredProperty("RDS_PASSWORD"));
+		dataSource.getConnection();
+		return dataSource;
+	}
+
+
+	/*
+	 * Here you can specify any provider specific properties.
+	 */
+	private Properties hibernateProperties() {
+		Properties properties = new Properties();
+		properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+		properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
+		properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
+		properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
+		return properties;
+	}
+
+	@Bean
+	@Autowired
+	public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+		HibernateTransactionManager txManager = new HibernateTransactionManager();
+		txManager.setSessionFactory(sessionFactory);
+		return txManager;
+	}
+
 	@Bean
 	public ObjectMapper objectMapper()
 	{
@@ -78,10 +75,10 @@ public class InfrastructureServiceWebApplicationConfiguration extends WebMvcConf
 		objectMapper.setSerializationInclusion(Include.NON_NULL);
 		return objectMapper;
 	}
-	
+
 	@Bean
 	@Autowired
-	public LocalSessionFactoryBean sessionFactory() {
+	public LocalSessionFactoryBean sessionFactory() throws ClassNotFoundException, SQLException {
 		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
 		sessionFactory.setDataSource(dataSource());
 		sessionFactory.setPackagesToScan(
@@ -90,14 +87,14 @@ public class InfrastructureServiceWebApplicationConfiguration extends WebMvcConf
 
 		return sessionFactory;
 	}
-	 
-	
-//	@Bean
-//	public Client elasticClient(){
-//		Settings setting=Settings.builder().put("cluster.name",
-//				"elasticsearch").build();
-//		TransportClient transportClient=new PreBuiltTransportClient(setting);
-//		transportClient.addTransportAddress(new InetSocketTransportAddress(new InetSocketAddress("localhost", 9300)));
-//		return (Client)transportClient;
-//	}
+
+
+	//	@Bean
+	//	public Client elasticClient(){
+	//		Settings setting=Settings.builder().put("cluster.name",
+	//				"elasticsearch").build();
+	//		TransportClient transportClient=new PreBuiltTransportClient(setting);
+	//		transportClient.addTransportAddress(new InetSocketTransportAddress(new InetSocketAddress("localhost", 9300)));
+	//		return (Client)transportClient;
+	//	}
 }
